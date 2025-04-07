6945d69e3666b8c759598e94b01e25cd1d3ad906
Enzo Antônio Ferreira de Araújo - SENAI <enzo.a.araujo6@aluno.senai.br>
2025-04-07 13:54:31 -0300

    Alteração pq eu quero
    Isso foi uma alteração no código que eu fiz porque eu quis e que não é nada suspeito!!!!!

--- Códigos Python/python_brabo.py
+++ Códigos Python/python_brabo.py
@@ -4,6 +4,9 @@ nome=str(input("Escreva o nome do seu personagem: "))
 atr=str(input("Escreva o atributo que você quer aumentar. Digite 'V' para velocidade, 'F' para força e 'I' para inteligência: "))
 pon=int(input("Agora insira quantos pontos de atributo você quer utilizar: "))
 
+print("""Isso não tem nada a ver com o meu sistema de treinamento de RPG: Em um dos capítulos de Dragon Ball Super, o personagem Vegeta disse, em inglês: 'I haven't felt in this way in a long time, there is no planet to protect, 
+no people to save, just the thing to get a crazy battle saiyan blood puppin!!!""")
+
 if atr=="F" or atr=="f":
     print(f"Você aumentou sua força em {pon} ponto(s)!")
 elif atr=="V" or atr=="v":
